6d90872b3c1183a99113a78d0653ec657237871a
LUIS ANGEL ESCOBAR CONSTANZA <SMSS144922@ugb.edu.sv>
2022-07-31 20:45:05 -0300

    programa de la divison

---
+++ app de la tarea 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace app_de_la_tarea_2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Ingrese un numero:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero:");
+            int num2 = int.Parse(Console.ReadLine());
+            var multiplicaciòn = num1 * num2;
+            Console.WriteLine("La multiplicacion de los numeros es: {0}", multiplicaciòn);
+
+        }
+        
+        }
+    }
+
---
+++ app de la tarea 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace app_de_la_tarea_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero:");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero:");
+            int num2 = int.Parse(Console.ReadLine());
+            var division = num1 / num2;
+            Console.WriteLine("La division de los numeros es: {0}", division);
+          
+        }
+    }
+}
