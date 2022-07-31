10cf1f6479e31342ac0570b8e64d2ce0c9bb1cc8
LUIS ANGEL ESCOBAR CONSTANZA <SMSS144922@ugb.edu.sv>
2022-07-31 19:17:00 -0300

    actualizacion

--- App2/Program.cs
+++ App2/Program.cs
@@ -11,6 +11,7 @@ namespace App2
             var suma = num1 + num2;
             int edad = 34;
             string name = "Luis";
+            //impresion de datos
             Console.WriteLine(suma);
             Console.WriteLine("Su nombre es: {0} y su edad es: {1}", name, edad);
         }
