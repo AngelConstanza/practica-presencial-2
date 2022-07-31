2180b43a8539ac9a76fa1dc142cacbe180053988
PC <PC@DESKTOP-EKG9S9D>
2022-07-31 18:55:55 -0300

    Agregar archivos de proyecto.

---
+++ practica presencial 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace practica_presencial_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            int edad = 34;
+            string name = "Luis";
+        
+            Console.WriteLine("su nombre es: " + name + " y su edad es: " + edad);
+        }
+    }
+}
