f550673eb720d526442765f87bb768e04db817fd
LUIS ANGEL ESCOBAR CONSTANZA <SMSS144922@ugb.edu.sv>
2022-07-31 19:32:01 -0300

    cuarto programa

--- App3/Program.cs
+++ App3/Program.cs
@@ -6,7 +6,10 @@ namespace App3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float num1 = 25.85F;
+            double num2 = 25.85;
+            decimal num3 = 25.85M;
+            Console.WriteLine("{0} {1} {2}", num1, num2, num3);
         }
     }
 }
